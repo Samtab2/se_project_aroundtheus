7f030b98e37cd61b193e3cf6d35ffb2e6257b2ed
Sammy Tabally <sammytabally@hotmail.com>
2024-01-03 13:48:57 -0600

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -40,7 +40,7 @@ const profileEditForm = profileEditModal.querySelector(".modal__form");
 const cardListEl = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
-const addNewCardButton = document.querySelector("profile-add-button");
+const addNewCardButton = document.querySelector("#profile-add-button");
 const addCardModal = document.querySelector("#add-card-modal");
 const addCardModalCloseButton = addCardModal.querySelector(
   "#add-modal-close-button"
